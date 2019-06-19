d1d761f86ee63fe3f184c298aadee145e57fc4ee
Dipmalya <dipmalyasen@gmail.com>
2019-06-20 01:02:37 +0530

    Babel-EsLint configured.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const title = "My Minimal React Webpack Babel Setup";
+const title = "A Sample React Application";
 
 ReactDOM.render(
     <div>
