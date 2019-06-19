316f80271721a2e38cad43c409b4f7993a4e59fc
Dipmalya <dipmalyasen@gmail.com>
2019-06-20 01:28:00 +0530

    ESLint features modified.

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,35 @@
-const webpack = require('webpack');
-
+const webpack = require("webpack");
 module.exports = {
-    entry: './src/index.jsx',
-    module: {
-        rules: [
+    "devServer": {
+        "contentBase": "./dist",
+        "hot": true
+    },
+    "entry": "./src/index.jsx",
+    "module": {
+        "rules": [
             {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": ["babel-loader"]
             },
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['eslint-loader']
+                "test": /\.js$/,
+                "exclude": /node_modules/,
+                "use": ["eslint-loader"]
             }
         ]
     },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
+    "resolve": {
+        "extensions": [
+            "*",
+            ".js",
+            ".jsx"
+        ]
     },
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
+    "output": {
+        "path": `${__dirname}/dist`,
+        "publicPath": "/",
+        "filename": "bundle.js"
     },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    }
+    "plugins": [new webpack.HotModuleReplacementPlugin()]
 };
